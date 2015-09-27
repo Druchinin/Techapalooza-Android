eed942215754d99895a3aeee6dcb8e319cf5af3d
Druchinin <druchinin86@gmail.com>
2015-09-28 01:22:29 +0300

    project created

---
+++ app/src/main/java/com/consultica/techapalooza/MainActivity.java
@@ -0,0 +1,26 @@
+package com.consultica.techapalooza;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppDefault);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Techapalooza Beta");
+    }
+
+
+}
